c48429967f499ec8e4be6e56238187a581d2372e
Felix Scholzen <felix.scholzen27@gmail.com>
2024-04-04 18:52:30 +0200

    1 angefangen

--- Uebung1/Aufgabe1/Aufgabe1/Aufgabe1.c
+++ Uebung1/Aufgabe1/Aufgabe1/Aufgabe1.c
@@ -6,12 +6,48 @@
  */ 
 
 
+#define F_CPU 16UL
 #include <avr/io.h>
+#include <util/delay.h>
+
 
 int main(void)
 {
+	
+	//DDRB = 0x07; // Set first 3 Pins of Port B as outputs
+	
+	DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2);
+
     while(1)
-    {
-        //TODO:: Please write your application code 
+    {	
+		
+		for(short counter = 0x00; counter <= 0x07; counter++){
+
+			PORTB = (0xFF ^ counter)^0xFF;
+			
+			//_delay_ms(1000);   
+		}
+	   
     }
 }
+
+
+
+
+
+
+/*
+		 while(counter >= 0){
+		   
+			
+			short mask = 0xFF << counter;
+		
+			PORTB = mask;
+			
+			counter--;
+			
+		
+			_delay_ms(300);   
+		}
+
+*/
