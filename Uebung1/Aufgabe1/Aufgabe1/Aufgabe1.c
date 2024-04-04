3b06a6f36f0cbdcf857a70b438814a966f45fcc6
Felix Scholzen <felix.scholzen27@gmail.com>
2024-04-04 17:16:19 +0200

    initial commit

---
+++ Uebung1/Aufgabe1/Aufgabe1/Aufgabe1.c
@@ -0,0 +1,17 @@
+/*
+ * Aufgabe1.c
+ *
+ * Created: 04.04.2024 17:15:27
+ *  Author: scf32306
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
